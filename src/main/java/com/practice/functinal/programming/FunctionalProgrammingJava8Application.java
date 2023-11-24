eec93c74144c962c3d8a1521f7a37553e3a0a580
unknown <vajjabageeradha@gmail.com>
2023-11-24 19:17:00 +0300

    First Commit Functional Programming

---
+++ src/main/java/com/practice/functinal/programming/FunctionalProgrammingJava8Application.java
@@ -0,0 +1,13 @@
+package com.practice.functinal.programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalProgrammingJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalProgrammingJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/functinal/programming/FunctionalProgrammingJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.functinal.programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgrammingJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
